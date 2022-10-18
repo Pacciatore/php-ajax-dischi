97d2dc8983eb7d2ddc5a6231276dc338e8f4b277
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-18 10:36:34 +0200

    js di index-vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log('JS OK');
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            albums: [],
+            filter: ''
+        }
+    },
+    mounted() {
+        this.getAlbums('');
+    },
+    methods: {
+        getAlbums(genre) {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then(response => {
+                    if (response.status === 200) {
+                        console.log(response);
+                        this.albums = response.data.response;
+                    }
+                })
+                .catch(e => console.log(e));
+
+
+        }
+    },
+
+})
