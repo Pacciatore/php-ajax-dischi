482e75d9ef52c7b549dcca3e1855e09aa503efdf
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-18 09:50:06 +0200

    cerazione database provvisorio

---
+++ database.php
@@ -0,0 +1,33 @@
+<?php
+
+$database = [
+    [
+        'title' => 'Killer',
+        'author' => 'Iron Maiden',
+        'year' => 1981,
+        'genre' => 'Metal',
+        'poster' => ''
+    ],
+    [
+        'title' => 'Hello',
+        'author' => 'The Beatles',
+        'year' => 1995,
+        'genre' => 'Pop',
+        'poster' => ''
+    ],
+    [
+        'title' => 'Jake',
+        'author' => 'Metallica',
+        'year' => 1978,
+        'genre' => 'Metal',
+        'poster' => ''
+    ],
+    [
+        'title' => 'Killer Bomb',
+        'author' => 'The Queen',
+        'year' => 1991,
+        'genre' => 'Pop',
+        'poster' => ''
+    ],
+    
+    ];
