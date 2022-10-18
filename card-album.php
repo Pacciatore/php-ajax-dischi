2f7c89536b0e76b65206e9a53d9007e0df11310a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-18 09:59:15 +0200

    creazione di card-album per display album

---
+++ card-album.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="img-container">
+        <img src="<?= $album['poster'] ?>" alt="<?= $album['title'] ?>">
+    </div>
+
+    <h2><?= $album['title'] ?></h2>
+
+    <p><?= $album['year'] ?></p>
+    <p><?= $album['author'] ?></p>
+
+</div>
