6b3cb042f9425a67ee0190a9a2a92c6ee146ce1a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-18 10:36:53 +0200

    refactor index.php

--- index.php
+++ index.php
@@ -19,23 +19,7 @@
 <body>
 
     <?php
-
-    include __DIR__ . '/database.php';
-    $filter = '';
-
-    if (isset($_GET['filter'])) {
-        $filter = strtolower($_GET['filter']);
-        $data = [];
-
-        foreach ($database as $album) {
-            if (strlen($filter) === 0 || strtolower($album['genre']) == $filter) {
-                $data[] = $album;
-            }
-        }
-
-        $database = $data;
-    };
-
+    include __DIR__ . '/db-logic.php';
     ?>
 
     <header class="pt-4">
